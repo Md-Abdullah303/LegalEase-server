7a40fc46c2ec7c6c599789c63a58dc4e3f077555
Mohammad Abdullah <mdabdulla01715940008@gmail.com>
2026-06-19 15:30:49 +0600

    Conected with MongoDB succesfull

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5555;
@@ -8,16 +9,53 @@ const port = process.env.PORT || 5555;
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.MONGODB_URL;
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("legal-ease");
+    const userCollection = database.collection("user");
+
+    // user related API
+    // app.get("/api/users", async (req, res) => {
+    //   const cursor = userCollection.find();
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // });
+    // app.get("/api/users/:id", async (req, res) => {
+    //   const { id } = req.params;
+    //   const query = {
+    //     _id: new ObjectId(id),
+    //   };
+    //   const result = await userCollection.findOne(query);
+    //   res.send(result);
+    // });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-//
-// app.get("/api/users/:id", async (req, res) => {
-//   const { id } = req.params;
-//   const result = await usersCollections.findOne(query)
-// });
